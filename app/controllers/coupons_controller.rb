3fc21baa4d747dda54ea7a0a581ee5cff4d4d430
Mark Viti <+>
2022-06-24 17:22:36 -0400

    Done.

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,42 @@
+class CouponsController < ApplicationController
+ 
+  def index
+    @coupons = Coupon.all
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def new
+    @coupon = Coupon.new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.store = params[:coupon][:store]
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.save
+    redirect_to coupons_path
+  end
+
+  def edit
+    @coupon = Coupon.find(params[:id])
+  end
+
+  def update
+    @coupon = Coupon.find(params[:id])
+    @coupon.store = params[:coupon][:store]
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.save
+    redirect_to coupons_path
+  end
+
+  def destroy
+    @coupon.destroy
+    respond_to do |format|
+      format.html { redirect_to coupons_url, notice: 'Coupon was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+end
